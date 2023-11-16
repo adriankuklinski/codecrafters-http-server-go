3faa386534098ae554bbfa5d0257ce1c6246a33d
Adrian Kuklinski <adriankuklinski@pm.me>
2023-11-16 18:38:16 -0500

    feat: respond with http 200 response header

--- app/server.go
+++ app/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "bufio"
 	"fmt"
 	"net"
 	"os"
@@ -13,9 +14,28 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+    var conn net.Conn
+	conn, err = l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+    defer conn.Close()
+
+    reader := bufio.NewReader(conn)
+    buffer := make([]byte, 1024)
+    _, err = reader.Read(buffer)
+    if err != nil {
+		fmt.Println(err)
+        os.Exit(1)
+    }
+
+    response := "HTTP/1.1 200 OK\r\n\r\n"
+    _, err = conn.Write([]byte(response))
+    if err != nil {
+        fmt.Println("Error writing response:", err)
+    }
+
+    conn.Close()
 }
+
